5069463cb8ba5be855188177d6f0690dbf216007
AS <anggalabs@gmail.com>
2025-05-04 07:30:34 +0700

    update

--- FSharp/HelloWorld/Program.fs
+++ FSharp/HelloWorld/Program.fs
@@ -1,11 +1,13 @@
+﻿// kurokuro
+
 let printMessage name =
     printfn $"Hello there, {name}!"
 
-// 'names' is inferred to be a sequence of strings.
 let printNames names =
     for name in names do
         printMessage name
 
 let names = [ "Ana"; "Felipe"; "Emillia" ]
 printNames names
-                              
+
+//----------------------------------------------------------
